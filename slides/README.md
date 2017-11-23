557a6a28393c3082b9da66d34e5c75a3b8a2da7f
Michał Zając <michal.zajac@gmail.com>
2017-11-23 01:01:39 +0100

    Add slides

---
+++ slides/README.md
@@ -0,0 +1,13 @@
+# Git 101
+
+## Slides
+
+Just open up `index.html`
+
+## I want to change something
+
+0. Fetch this repository
+1. Install [Ruby](https://rvm.io/) and [Nokogiri](http://www.nokogiri.org/)
+2. Install [Pandoc](https://pandoc.org/)
+3. Make your changes to `slides.md`
+4. `make`
---
+++ slides/fix_images.rb
@@ -0,0 +1,18 @@
+require "nokogiri"
+require "pry"
+
+def move_image_and_remove_paragraph(image)
+  raise "No image given" unless image
+  to_remove = image.parent
+  section = to_remove.parent
+  section.add_child(image)
+  to_remove.remove
+end
+
+@doc = File.open("index.html") { |f| Nokogiri::HTML(f) }
+
+@doc.css(".stretch").each do |image|
+  move_image_and_remove_paragraph(image)
+end
+
+File.open("index.html", "w") { |f| f << @doc.to_html }
